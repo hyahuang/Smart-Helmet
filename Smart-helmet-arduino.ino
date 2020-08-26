f1f5ababf86286f098ffa992d93361495d41d775
hyahuang <69639309+hyahuang@users.noreply.github.com>
2020-08-26 15:56:19 +0800

    Create Smart-helmet-arduino.ino

---
+++ Smart-helmet-arduino.ino
@@ -0,0 +1,186 @@
+
+
+
+
+
+
+
+#include <SoftwareSerial.h>  
+SoftwareSerial BT(0,1);   // 接收腳(RX), 傳送腳(TX)；接HC-06之TXD、RXD   
+  
+char val;  
+String recieveData = "";   
+bool startRecieve = false;  
+
+const int AOUTpin=0;//the AOUT pin of the alcohol sensor goes into analog pin A0 of the arduino
+const int DOUTpin=8;//the DOUT pin of the alcohol sensor goes into digital pin D8 of the arduino
+const int ledPin=13;//the anode of the LED connects to digital pin D13 of the arduino
+
+int limit;
+int value;
+float sum;
+
+
+#include <SPI.h>
+#include <Wire.h>
+#include <ADXL345.h>
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
+ADXL345 adxl;
+
+
+
+
+void setup()  
+{  
+  Serial.begin(9600);   
+  BT.begin(9600); //HC-06 預設 baud  
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  adxl.powerOn();
+
+  //set activity/ inactivity thresholds (0-255)
+  adxl.setActivityThreshold(75); //62.5mg per increment
+  adxl.setInactivityThreshold(75); //62.5mg per increment
+  adxl.setTimeInactivity(10); // how many seconds of no activity is inactive?
+ 
+  //look of activity movement on this axes - 1 == on; 0 == off 
+  adxl.setActivityX(1);
+  adxl.setActivityY(1);
+  adxl.setActivityZ(1);
+ 
+  //look of inactivity movement on this axes - 1 == on; 0 == off
+  adxl.setInactivityX(1);
+  adxl.setInactivityY(1);
+  adxl.setInactivityZ(1);
+ 
+  //look of tap movement on this axes - 1 == on; 0 == off
+  adxl.setTapDetectionOnX(0);
+  adxl.setTapDetectionOnY(0);
+  adxl.setTapDetectionOnZ(1);
+ 
+  //set values for what is a tap, and what is a double tap (0-255)
+  adxl.setTapThreshold(50); //62.5mg per increment
+  adxl.setTapDuration(15); //625us per increment
+  adxl.setDoubleTapLatency(80); //1.25ms per increment
+  adxl.setDoubleTapWindow(200); //1.25ms per increment
+ 
+  //set values for what is considered freefall (0-255)
+  adxl.setFreeFallThreshold(7); //(5 - 9) recommended - 62.5mg per increment
+  adxl.setFreeFallDuration(45); //(20 - 70) recommended - 5ms per increment
+ 
+  //setting all interrupts to take place on int pin 1
+  //I had issues with int pin 2, was unable to reset it
+  adxl.setInterruptMapping( ADXL345_INT_SINGLE_TAP_BIT,   ADXL345_INT1_PIN );
+  adxl.setInterruptMapping( ADXL345_INT_DOUBLE_TAP_BIT,   ADXL345_INT1_PIN );
+  adxl.setInterruptMapping( ADXL345_INT_FREE_FALL_BIT,    ADXL345_INT1_PIN );
+  adxl.setInterruptMapping( ADXL345_INT_ACTIVITY_BIT,     ADXL345_INT1_PIN );
+  adxl.setInterruptMapping( ADXL345_INT_INACTIVITY_BIT,   ADXL345_INT1_PIN );
+ 
+  //register interrupt actions - 1 == on; 0 == off  
+  adxl.setInterrupt( ADXL345_INT_SINGLE_TAP_BIT, 1);
+  adxl.setInterrupt( ADXL345_INT_DOUBLE_TAP_BIT, 1);
+  adxl.setInterrupt( ADXL345_INT_FREE_FALL_BIT,  1);
+  adxl.setInterrupt( ADXL345_INT_ACTIVITY_BIT,   1);
+  adxl.setInterrupt( ADXL345_INT_INACTIVITY_BIT, 1);
+
+  pinMode(DOUTpin, INPUT);//sets the pin as an input to the arduino
+  pinMode(ledPin, OUTPUT);//sets the pin as an output of the arduino
+}  
+  
+void loop()  
+{  
+
+  int x,y,z;  
+  adxl.readXYZ(&x, &y, &z); //read the accelerometer values and store them in variables  x,y,z
+  // Output x,y,z values 
+  /*Serial.print("values of X , Y , Z: ");
+  Serial.print(x);
+  Serial.print(" , ");
+  Serial.print(y);
+  Serial.print(" , ");
+  Serial.println(z);*/
+  
+  double xyz[3];
+  double ax,ay,az;
+  adxl.getAcceleration(xyz);
+  ax = xyz[0];
+  ay = xyz[1];
+  az = xyz[2];
+ /* Serial.print("X=");
+  Serial.print(ax);
+  Serial.println(" g");
+  Serial.print("Y=");
+  Serial.print(ay);
+  Serial.println(" g");
+  Serial.print("Z=");
+  Serial.print(az);
+  Serial.println(" g");
+  Serial.println("**********************");*/
+  sum=ax+ay;
+  sum=sum+az;
+  if(sum>1||sum<-1)
+  {
+    Serial.print("2");
+  }
+
+
+  value= analogRead(AOUTpin);//reads the analaog value from the alcohol sensor's AOUT pin
+  limit= digitalRead(DOUTpin);//reads the digital value from the alcohol sensor's DOUT pin
+  //Serial.print("Alcohol value: ");
+  //Serial.println(value);//prints the alcohol value
+  //Serial.print("Limit: ");
+  //Serial.print(limit);//prints the limit reached as either LOW or HIGH (above or underneath)
+  delay(100);
+  if (value >= 600){
+  digitalWrite(ledPin, HIGH);//if limit has been reached, LED turns on as status 
+  Serial.print("1");
+  }
+  else{
+  digitalWrite(ledPin, LOW);//if threshold not reached, LED remains off
+  }
+
+  
+
+
+  while(BT.available()) //如果有收到資料  
+  {  
+    startRecieve = true;  
+    val=BT.read(); //每次接收一個字元  
+    recieveData += val; //字元組成字串  
+    delay(50);
+  }  
+    
+  if(startRecieve)  
+  {  
+  startRecieve = false;  
+  instruction(recieveData);
+  //Serial.print(recieveData);
+  recieveData = "";  
+  }
+  delay(300);
+}  
+
+
+
+void instruction(String receivedata){
+  display.clearDisplay();
+  Serial.print(recieveData);
+  display.setTextSize(1); // Draw 2X-scale text
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.println(receivedata);
+  display.display();      // Show initial text
+}
